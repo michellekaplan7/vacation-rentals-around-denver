36f116c1301726cc40457d9d64439d7ac994180b
emackinnon1 <emackinnon100390@gmail.com>
2020-05-12 16:31:28 -0600

    Create Areas component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Areas from "../Areas/Areas";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Hello</h1>
+			<Areas title="FUCK" />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Areas/Areas.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Areas.css";
+
+const Areas = (props) => {
+	return <h1>{props.title}</h1>;
+};
+
+export default Areas;
