7f7d28ded3dd1e9c42baa2523d29c2073cc0f4cf
emackinnon1 <emackinnon100390@gmail.com>
2020-05-12 21:02:41 -0600

    Created helper function that fetches area details and updates the state to hold all details and the each area nickname

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,62 @@
-import React from "react";
-import "./App.css";
-import Areas from "../Areas/Areas";
+import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
 
-function App() {
-	return (
-		<div className="App">
-			<h1>Hello</h1>
-			<Areas title="FUCK" />
-		</div>
-	);
+import Areas from "../Areas/Areas";
+
+class App extends Component {
+	state = {
+		url: "https://vrad-api.herokuapp.com",
+		areas: null,
+		listings: null,
+	};
+
+	componentDidMount() {
+		fetch("https://vrad-api.herokuapp.com/api/v1/areas")
+			.then((response) => response.json())
+			.then((data) => this.fetchAreaDetails(data.areas))
+			.then((areas) => this.setState({ areas }))
+			// .then((data) => this.setState({ areas: data.areas }))
+			.catch((err) => console.log(err));
+	}
+
+	fetchAreaDetails = (areaList) => {
+		const promises = areaList.map((area) => {
+			return fetch(this.state.url + area.details)
+				.then((response) => response.json())
+				.then((details) => {
+					return {
+						area: area.area,
+						...details,
+					};
+				});
+		});
+		return Promise.all(promises);
+	};
+
+	updateState = (areasInfo) => {};
+
+	// fetchListingDetails = (listings) => {
+	// 	const promises = listings.map((listing) => {
+	// 		return fetch(this.state.url + listing.details)
+	// 			.then((response) => response.json())
+	// 			.then(details);
+	// 	});
+	// };
+
+	render() {
+		return (
+			<BrowserRouter>
+				<div className="App">
+					{!this.state.areas ? (
+						<h1>Loading...</h1>
+					) : (
+						<Areas areas={this.state.areas} />
+					)}
+				</div>
+			</BrowserRouter>
+		);
+	}
 }
 
 export default App;
--- src/components/Areas/Areas.js
+++ src/components/Areas/Areas.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./Areas.css";
+import PropTypes from "prop-types";
 
 const Areas = (props) => {
-	return <h1>{props.title}</h1>;
+	const areaNames = props.areas.map((area, i) => {
+		return (
+			<div key={i}>
+				<h1>{area.area}</h1>
+				<h1>{area.name}</h1>
+			</div>
+		);
+	});
+	return <div>{areaNames}</div>;
 };
 
 export default Areas;
