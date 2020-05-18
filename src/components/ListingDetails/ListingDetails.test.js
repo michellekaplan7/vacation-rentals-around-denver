83f00165fbf1184bb0df0576aa208737c6aef8b0
emackinnon1 <emackinnon100390@gmail.com>
2020-05-18 15:44:03 -0600

    ListingDetails test is passing and displays correct info

---
+++ src/components/ListingDetails/ListingDetails.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ListingDetails from "./ListingDetails";
+import { MemoryRouter } from "react-router-dom";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+describe("Listing", () => {
+	it("should render a Listing card", () => {
+		const mockListingDetailsData = {
+			listing_id: 3,
+			name: "The Playboy Mansion",
+			cost_per_night: 10000,
+			areaId: 590,
+			address: "123 Yo Mama's House",
+			features: ["sex dungeon", "cocaine", "strippers"],
+			beds: 14,
+			baths: 1,
+			superhost: false,
+		};
+
+		const router = (
+			<MemoryRouter>
+				<ListingDetails {...mockListingDetailsData} />
+			</MemoryRouter>
+		);
+
+		const { getByText } = render(router);
+
+		expect(getByText("sex dungeon", { exact: false })).toBeInTheDocument();
+	});
+});
