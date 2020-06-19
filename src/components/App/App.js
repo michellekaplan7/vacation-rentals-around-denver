72a0d74a0a56769f0224bb31076eaaa5e6177334
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-06-18 20:35:42 -0600

    Add toggleFavorites method and pass as props to ListingDetails, as well as pass favoritesID to Header and format document

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,86 +10,117 @@ import ListingDetails from "../ListingDetails/ListingDetails";
 import Header from "../Header/Header";
 
 class App extends Component {
-	state = {
-		userInfo: {},
-		url: "https://vrad-api.herokuapp.com",
-		areas: [],
-		selectedListing: {},
-		favorites: [],
-	};
+  state = {
+    userInfo: {},
+    url: "https://vrad-api.herokuapp.com",
+    areas: [],
+    selectedListing: {},
+    favorites: [],
+    favoritesID: [],
+  };
 
-	componentDidMount = async () => {
-		const areas = await getAreas();
-		this.setState({ areas });
-	};
+  componentDidMount = async () => {
+    const areas = await getAreas();
+    this.setState({ areas });
+  };
 
-	selectListing = (selectedListing) => {
-		this.setState({ selectedListing });
-	};
+  selectListing = (selectedListing) => {
+    this.setState({ selectedListing });
+  };
 
-	handleUserInfo = (userInfo) => {
-		this.setState({ userInfo });
-	};
+  handleUserInfo = (userInfo) => {
+    this.setState({ userInfo });
+  };
 
-	addToFavorites = (listing) => {
-		this.setState = { favorites: [...this.state.favorites, listing] };
-	};
+  toggleFavorites = (id) => {
+    if (!this.state.favoritesID.includes(id)) {
+      this.setState({ favoritesID: [...this.state.favoritesID, id] }, () => {
+        // this.updateLocalStorage()
+      });
+    } else {
+      let newFavoritesID = this.state.favoritesID.filter((favorite) => {
+        return favorite !== id;
+      });
+      let newFavorites = this.state.favorites.filter((favorite) => {
+        return favorite.id !== id;
+      });
+      this.setState(
+        { favoritesID: newFavoritesID, favorites: newFavorites },
+        () => {
+          // this.updateLocalStorage()
+        }
+      );
+    }
+  };
 
-	render() {
-		return (
-			<main className="app">
-				<Switch>
-					<Route
-						path="/areas/:id/listings/:listingId"
-						render={({ match }) => {
-							return (
-								<div className="body-container">
-									<Header userInfo={this.state.userInfo} />
-									<ListingDetails {...this.state.selectedListing} />
-								</div>
-							);
-						}}
-					/>
-					<Route
-						path="/areas/:id/listings"
-						render={({ match }) => {
-							const areaId = Number(match.params.id);
-							const selectedArea = this.state.areas.find(
-								(area) => areaId === area.id
-							);
-							return (
-								<div className="body-container">
-									<Header userInfo={this.state.userInfo} />
-									<Listings
-										match={match.params.id}
-										{...selectedArea}
-										selectListing={this.selectListing}
-									/>
-								</div>
-							);
-						}}
-					/>
-					<Route
-						exact
-						path="/areas"
-						render={() => {
-							return (
-								<div className="body-container">
-									<Header userInfo={this.state.userInfo} />
-									<Areas areas={this.state.areas} />
-								</div>
-							);
-						}}
-					/>
-					<Route
-						exact
-						path="/"
-						render={() => <WelcomeForm handleUserInfo={this.handleUserInfo} />}
-					/>
-				</Switch>
-			</main>
-		);
-	}
+  render() {
+    return (
+      <main className="app">
+        <Switch>
+          <Route
+            path="/areas/:id/listings/:listingId"
+            render={({ match }) => {
+              return (
+                <div className="body-container">
+                  <Header
+                    userInfo={this.state.userInfo}
+                    favoritesID={this.state.favoritesID}
+                  />
+                  <ListingDetails
+                    {...this.state.selectedListing}
+                    toggleFavorites={this.toggleFavorites}
+                    favoritesID={this.state.favoritesID}
+                  />
+                </div>
+              );
+            }}
+          />
+          <Route
+            path="/areas/:id/listings"
+            render={({ match }) => {
+              const areaId = Number(match.params.id);
+              const selectedArea = this.state.areas.find(
+                (area) => areaId === area.id
+              );
+              return (
+                <div className="body-container">
+                  <Header
+                    userInfo={this.state.userInfo}
+                    favoritesID={this.state.favoritesID}
+                  />
+                  <Listings
+                    match={match.params.id}
+                    {...selectedArea}
+                    selectListing={this.selectListing}
+                  />
+                </div>
+              );
+            }}
+          />
+          <Route
+            exact
+            path="/areas"
+            render={() => {
+              return (
+                <div className="body-container">
+                  <Header
+                    userInfo={this.state.userInfo}
+                    favoritesID={this.state.favoritesID}
+                  />
+                  <Areas areas={this.state.areas} />
+                </div>
+              );
+            }}
+          />
+          <Route
+            exact
+            path="/"
+            render={() => <WelcomeForm handleUserInfo={this.handleUserInfo} />}
+          />
+        </Switch>
+      </main>
+    );
+  }
 }
 
 export default App;
